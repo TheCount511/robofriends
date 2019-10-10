f7f1793d7ada1da7c20fa017b364d529c26e6607
Fadhil <fadhil.akins@gmail.com>
2019-10-10 11:12:29 +0100

    added new files

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+
+    return (
+     <div className="bg-light-green dib br3 pa3 ma2 grow bw1 shadow-1">
+        <img alt ='robots' src='https://robohash.org/set_set3/?size=100x100'/>
+        <div>
+        <h2> Jane Doe </h2> 
+        <p> jane.doe @gmail.com </p> 
+        </div> 
+        </div>
+        );
+}
+
+export default Card;
