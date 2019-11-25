fd764b1f562d6b987afb098999e8109a48ca2324
Fadhil <fadhil.akins@gmail.com>
2019-11-25 14:15:45 +0100

    changes made, project built

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Card from './Card'
 
-const CardList = ({robots}) => {return (<div>{robots.map((user,i) => {
+const CardList = ({robots}) => {
+	return (<div>{robots.map((user,i) => {
 		
 		return (<Card 
 		key={i} 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
+import ErrorBoundary from '../components/ErrorBoundary';
 import Scroll from '../components/Scroll';
 
 
@@ -17,27 +18,27 @@ class App extends Component {
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => this.setState({robots: users}));
 
     }
- 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
+
+    onSearchChange = (event) => {this.setState({ searchfield: event.target.value})
 
     }
-    render() {
-        const { robots, searchfield } = this.state
-        const filteredRobots = robots.filter(robot => {
-            return robots.name.toLowerCase().includes(searchfield.toLowerCase());
+    render() {const { robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
 
         return !robots.length ? <h1> Loading </h1> :
-            <div className= 'tc'>
+            <div className='tc'>
         <h1 className='f2'>RoboFriends</h1>
         <SearchBox searchChange = {this.onSearchChange}/>
         <Scroll>
+        <ErrorBoundary>
         <CardList robots= {filteredRobots}/>
+        </ErrorBoundary>
         </Scroll>
+
         </div>
 
     }
