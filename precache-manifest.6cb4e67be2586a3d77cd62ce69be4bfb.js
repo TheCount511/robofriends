611850afb10d37f135fb2479e8c3aa98da877658
Fadhil <fadhil.akins@gmail.com>
2019-11-25 14:59:03 +0100

    Updates

---
+++ precache-manifest.6cb4e67be2586a3d77cd62ce69be4bfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5647caf3ac241c86cb5aa26943cc81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "48a441d20a6785d2776a",
+    "url": "/static/css/main.5b0ae842.chunk.css"
+  },
+  {
+    "revision": "b77b6743426717f7caa2",
+    "url": "/static/js/2.f9b2e496.chunk.js"
+  },
+  {
+    "revision": "48a441d20a6785d2776a",
+    "url": "/static/js/main.98107aa4.chunk.js"
+  },
+  {
+    "revision": "05efcc9d811b4a2a3c6e",
+    "url": "/static/js/runtime-main.8f8b62bf.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/static/media/SEGA.82e09f78.TTF"
+  }
+]);
